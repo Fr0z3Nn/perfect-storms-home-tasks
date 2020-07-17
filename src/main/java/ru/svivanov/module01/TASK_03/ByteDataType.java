77c23f98678c594062309389e0a61aaf5e179b01
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-18 02:12:29 +0300

    int -> byte task

---
+++ src/main/java/ru/svivanov/module01/TASK_03/ByteDataType.java
@@ -0,0 +1,12 @@
+package ru.svivanov.module01.TASK_03;
+
+public class ByteDataType {
+    //TODO Write a fragment of the class (group of commands) that will assign the first value: ​​127, and the second: 255,
+    // and then the third variable will receive a value equal to the substraction of the values ​​of the two variables.
+    // The result should be with a byte data type.
+    public static void main(String[] args) {
+       int x = 255;
+       int y = 127;
+       byte z = (byte) (x - y);
+    }
+}
