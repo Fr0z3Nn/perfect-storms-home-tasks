84335944b943f07b4ae76e01a2e926136e5f5130
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-21 13:33:27 +0300

    long task

---
+++ src/main/java/ru/svivanov/module01/TASK_03/LongTask.java
@@ -0,0 +1,11 @@
+package ru.svivanov.module01.TASK_03;
+
+public class LongTask {
+    //TODO Print to the console the maximum value that can be of type long. Answer the question,
+    // where such data type can be used?
+    public static void main(String[] args) {
+        System.out.println(Long.MAX_VALUE);
+    }
+    /*it can be used in counting of people's population of the earth,
+    in time recording(UNIX 2038year problem)*/
+}
