1ae1b5b40e30f8aff9a03e7550492e9d055ede8e
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-21 14:02:55 +0300

    chaaaaaaaaar

---
+++ src/main/java/ru/svivanov/module01/TASK_03/CharTask.java
@@ -0,0 +1,17 @@
+package ru.svivanov.module01.TASK_03;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+import java.util.List;
+import java.util.stream.IntStream;
+
+
+public class CharTask {
+    //TODO Write a class which displays the character in the first line “+”,
+    // In the second - the sign “!”, In the third - the sign “?”.
+    public static void main(String[] args) {
+        char[] list = {'+','!','?'};
+        IntStream.range(0,list.length).mapToObj(i -> list[i]).forEach(System.out::println);
+    }
+    // char data type stores 65535 values, it takes 2 byte(32bit).
+}
