78b75cfa4cb93293d4bcd1557928c2dc36630c67
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-21 14:22:07 +0300

    data types

---
+++ src/main/java/ru/svivanov/module01/TASK_03/AllData.java
@@ -0,0 +1,19 @@
+package ru.svivanov.module01.TASK_03;
+
+public class AllData {
+    //TODO Below are the lines of the code. Mark those that will be compiled without errors.
+    public static void main(String[] args) {
+        int x = 'r';
+        char y = 123;
+        //float z = 7,2;
+        //short f = 3L;
+        double v = 0x123D;
+        int p = 0x123F;
+        //int o = 078;
+        short t = 7;
+        //byte n = 255;
+        double r = 9;
+        //int q = 5,5;
+        int m = 5;
+    }
+}
