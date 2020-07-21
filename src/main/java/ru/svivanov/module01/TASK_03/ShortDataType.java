cb1258612ff843ee4ed34f70681cda5b7af2296b
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-21 13:12:24 +0300

    easy short task

--- src/main/java/ru/svivanov/module01/TASK_03/ShortDataType.java
+++ src/main/java/ru/svivanov/module01/TASK_03/ShortDataType.java
@@ -1,4 +1,13 @@
 package ru.svivanov.module01.TASK_03;
 
 public class ShortDataType {
+    //TODO Write a fragment of the class (group of commands) that will assign the values ​​-32768 and 32768 to two
+    // variables, and then the third variable will receive a value equal to the sum of the values ​​of the two variables.
+    // The result should be with a short data type.
+    public static void main(String[] args) {
+        short x = -32768;
+        short y = (short)32768;//-32768
+        System.out.println((byte)(x-y)); //-32768-(-32768)==0
+    }
+    // short data type stores the numbers of range -32768 to 32767, it takes 2 byte.
 }
