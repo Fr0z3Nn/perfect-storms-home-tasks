9924260365a503bc2aa7bcd519ad4129ddd20c7e
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-23 20:20:16 +0300

    add stream to task

--- src/main/java/ru/svivanov/module01/TASK_05/IfElseIfStatement.java
+++ src/main/java/ru/svivanov/module01/TASK_05/IfElseIfStatement.java
@@ -1,11 +1,18 @@
 package ru.svivanov.module01.TASK_05;
 
+import java.util.stream.IntStream;
+
 public class IfElseIfStatement {
+    private static void streamMinMax(int A, int B, int C){
+        System.out.printf("MIN = %s\n", IntStream.of(A,B,C).min().getAsInt());
+        System.out.printf("MAX = %s\n", IntStream.of(A,B,C).max().getAsInt());
+    }
 
     public static void main(String[] args) {
         int a = 32;
         int b = 2;
         int c = 78;
+        //streamMinMax(a,b,c);
         if (a>b && a>c){
 
             if(b>c){
