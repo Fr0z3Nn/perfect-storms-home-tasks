3b570e612fa7387509cbe66ae639645b7acf93d5
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-23 20:34:28 +0300

    ternary print

---
+++ src/main/java/ru/svivanov/module01/TASK_05/Ternarny.java
@@ -0,0 +1,17 @@
+package ru.svivanov.module01.TASK_05;
+
+public class Ternarny {
+    //TODO Replace "if-else" construction to ternary operator.
+    //
+    //int num = 8;
+    //String message = "";
+    //if(num > 10) {
+    //    message = "Number is greater than 10";
+    //} else {
+    //    message = "Number is less than or equal to 10";
+    //}
+    public static void main(String[] args) {
+        int num = 8;
+        System.out.println(num > 10 ? "Number is greater than 10" : "Number is less than or equal to 10");
+    }
+}
