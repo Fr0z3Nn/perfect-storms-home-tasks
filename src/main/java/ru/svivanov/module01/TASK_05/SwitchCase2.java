5dd432b0fe2481f320c77d254b324504a7d38e95
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-24 12:52:49 +0300

    break == fix

---
+++ src/main/java/ru/svivanov/module01/TASK_05/SwitchCase2.java
@@ -0,0 +1,38 @@
+package ru.svivanov.module01.TASK_05;
+
+import java.util.Scanner;
+
+public class SwitchCase2 {
+    //TODO Fix the code to get correct console output.
+    public static void main(String[] args) {
+        System.out.println("Enter the day of the week.");
+        Scanner scanner = new Scanner(System.in);
+        String day = scanner.next().toLowerCase(); // it can be any day
+
+        switch(day) {
+            case "monday":
+                System.out.println("This is the first day of the week");
+                break;
+            case "tuesday":
+                System.out.println("This is the second day of the week");
+                break;
+            case "wednesday":
+                System.out.println("This is the third day of the week");
+                break;
+            case "thursday":
+                System.out.println("This is the fourth day of the week");
+                break;
+            case "friday":
+                System.out.println("This is the fifth day of the week");
+                break;
+            case "saturday":
+                System.out.println("This is the sixth day of the week");
+                break;
+            case "sunday":
+                System.out.println("This is the seventh day of the week");
+                break;
+            default:
+                System.out.println("Such day of the week doesn't exist.");
+        }
+    }
+}
