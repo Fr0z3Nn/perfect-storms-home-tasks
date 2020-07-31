a11c59acaccd18763cc5d7c00a9afcfa17d131c0
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-31 16:58:01 +0300

    add 1-5,7 of menu

--- src/main/java/ru/svivanov/module01/FINAL_TASK/menu/Movie.java
+++ src/main/java/ru/svivanov/module01/FINAL_TASK/menu/Movie.java
@@ -1,45 +1,68 @@
 package ru.svivanov.module01.FINAL_TASK.menu;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Movie {
     public static byte[][] MovieList = new byte[100][1000];
 
     private static final byte[] EmptyList = new byte[1000];
 
-    public static void addMovie(byte[] movie){
+    public static boolean add(byte[] movie){
+        if(check(movie)){
+            return false;
+        }
         for (int i=0; i < MovieList.length; i++){
             if (Arrays.equals(MovieList[i],EmptyList)){
                 MovieList[i] = movie;
-                return;
+                break;
             }
-        }
+        }return true;
     }
 
-    public static void removeMovie(byte[] movie){
+    public static boolean remove(byte[] movie){
+        if(!check(movie)){
+            return false;
+        }
         for (int i=0; i < MovieList.length; i++){
             if (Arrays.equals(MovieList[i],movie)){
                 MovieList[i] = EmptyList;
-                return;
+                break;
+            }
+        }return true;
+    }
+
+    public static boolean edit(byte[] movie){
+        if(!check(movie)){
+            return false;
+        }
+        System.out.println("Write new name of the movie");
+        Scanner scanner = new Scanner(System.in);
+        byte[] newMovieTitle = scanner.nextLine().toLowerCase().getBytes();
+        remove(movie);
+        add(newMovieTitle);
+        return true;
+    }
+
+    public static void print(){
+        System.out.println("LIST OF MOVIES:");
+        for (byte[] bytes : MovieList) {
+            if (!Arrays.equals(bytes, EmptyList)) {
+                System.out.println(new String(bytes));
             }
         }
     }
 
-    public static void printMovies(){
-        for(int i=0; i < MovieList.length; i++){
-            if (!Arrays.equals(MovieList[i],EmptyList)){
-                System.out.println(Arrays.toString(MovieList[i]));
-            }
-        }
-    }
-
-    public static void checkMovie(byte[] movie){
+    public static boolean check(byte[] movie){
         boolean isListContainMovie = false;
-        for (int i=0; i < MovieList.length; i++){
-            if (Arrays.equals(MovieList[i],movie)){
+        for (byte[] bytes : MovieList) {
+            if (Arrays.equals(bytes, movie)) {
                 isListContainMovie = true;
             }
-        }
-        System.out.println(isListContainMovie ? "Данный фильм есть в базе" : "Данного фильма нет в базе");
+        } return isListContainMovie;
+    }
+
+    public static void sort(){
+        //SORT
     }
 }
--- src/main/java/ru/svivanov/module01/FINAL_TASK/menu/Structure.java
+++ src/main/java/ru/svivanov/module01/FINAL_TASK/menu/Structure.java
@@ -1,8 +1,21 @@
 package ru.svivanov.module01.FINAL_TASK.menu;
 
+import java.util.Scanner;
+
 public class Structure {
+    public static void instruction(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("WELCOME TO THE MOVIE LIST PROGRAM.\n" +
+                "Instruction manual:\n" +
+                "1. Navigate through the menu items by entering numbers [1-7].\n" +
+                "2. This program can store up to 100 movies.\n" +
+                "\n" +
+                "PRESS ENTER to continue...");
+        scanner.nextLine();
+    }
     public static void showMenu(){
         String[] menuItemForShow = {
+                "       ACTION LIST",
                 "1. Add a movie name.",
                 "2. Remove the name of the movie.",
                 "3. Edit the name of the movie.",
