4e9e131fff7c741b9c81904068ea8ec74ba65535
Fr0z3Nn <luckyjohn057@gmail.com>
2020-07-31 17:01:16 +0300

    add main method

--- src/main/java/ru/svivanov/module01/FINAL_TASK/Main.java
+++ src/main/java/ru/svivanov/module01/FINAL_TASK/Main.java
@@ -1,17 +1,19 @@
 package ru.svivanov.module01.FINAL_TASK;
 
-import ru.svivanov.module01.FINAL_TASK.menu.Movie;
 import ru.svivanov.module01.FINAL_TASK.menu.Structure;
-
-import java.util.Arrays;
+import java.util.Scanner;
+import ru.svivanov.module01.FINAL_TASK.menu.UserChoice;
 
 
 public class Main {
     public static void main(String[] args) {
-        Structure.showMenu();
-        Movie.addMovie("2344".getBytes());
-        Movie.printMovies();
-        //Movie.removeMovie("2344".getBytes());
-        Movie.printMovies();
+        Scanner scanner = new Scanner(System.in);
+
+        Structure.instruction();
+
+        while (true){
+            Structure.showMenu();
+            UserChoice.choice(scanner.nextInt());
+        }
     }
 }
