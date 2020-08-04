6ac57b74d60a55e9ed5958b1e60b1c1baa4c5759
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-04 11:53:35 +0300

    task 2

---
+++ src/main/java/ru/svivanov/module01/TASK_09/Task_02.java
@@ -0,0 +1,10 @@
+package ru.svivanov.module01.TASK_09;
+
+public class Task_02 {
+    //TODO You have a phrase "London - is a capital of England".
+    // You need to change this phrase to "London - is a capital of Great Britain".
+    public static void main(String[] args) {
+        String s = "London - is a capital of England";
+        String sNew = s.replace("England","Great Britain");
+    }
+}
