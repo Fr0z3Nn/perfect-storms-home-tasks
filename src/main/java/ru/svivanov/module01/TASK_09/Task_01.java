42d14c35e64a17a0a821b0193ce3fdbcffac9af4
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-04 11:47:10 +0300

    answer: 2

--- src/main/java/ru/svivanov/module01/TASK_09/Task_01.java
+++ src/main/java/ru/svivanov/module01/TASK_09/Task_01.java
@@ -1,4 +1,15 @@
 package ru.svivanov.module01.TASK_09;
 
 public class Task_01 {
+    public static void main(String[] args) {
+        //TODO How many objects will be in a String Pool?
+        String st;
+        st = "Java"; // in pool (1)
+        String st2 = new String("Java"); // new object out of pool  (1)
+        String st3 = st2.intern(); // in pool, but Java is already here (1)
+        String st4 = "java"; // in pool (2)
+        st = null; // st -> null, but Java in pool.(2)
+        String st6 = new String("JAVA"); // new object out of pull (2)
+        //ANSWER: 2
+    }
 }
