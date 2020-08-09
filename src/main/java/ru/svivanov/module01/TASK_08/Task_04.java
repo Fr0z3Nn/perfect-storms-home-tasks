abc6e1a286c364151d314ac0b2e4994a02383393
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-09 18:47:12 +0300

    add fib func

--- src/main/java/ru/svivanov/module01/TASK_08/Task_04.java
+++ src/main/java/ru/svivanov/module01/TASK_08/Task_04.java
@@ -5,9 +5,23 @@ public class Task_04 {
         //TODO Write a method that takes an integer parameter and displays numbers from 0 to the parameter. Use recursion. Call it in the main method.
 
         recursionPrint(5);
+        for(int i =0;i<10;i++){
+            System.out.println(fib(i));
+        }
 
     }
+    // 0 1 1 2 3 5 8 13
+    private static long fib(int index){
+        if (index == 0){
+            return 0;
+        }else if(index == 1){
+            return 1;
+        }else{
+            return fib(index-1)+ fib(index-2);
+        }
 
+
+    }
     private static void recursionPrint(int num) {
         if (num < 0) {
             return;
