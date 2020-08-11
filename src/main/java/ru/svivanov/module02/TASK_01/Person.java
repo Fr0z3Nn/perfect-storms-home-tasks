1a395ced5e1b71454225b69f7b9d0278e34b2fe0
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-11 21:28:36 +0300

    add meditation method

--- src/main/java/ru/svivanov/module02/TASK_01/Person.java
+++ src/main/java/ru/svivanov/module02/TASK_01/Person.java
@@ -1,5 +1,7 @@
 package ru.svivanov.module02.TASK_01;
 
+import java.util.Date;
+
 public class Person {
     private String name;
     private int age;
@@ -7,6 +9,14 @@ public class Person {
     public Person(String name, int age){
         this.name = name;
         this.age = age;
+    }
+    public void meditation(){
+        System.out.print("I think - it means I exist. " + this +" "+ new Date());
+    }
+
+    @Override
+    public String toString() {
+        return "(" + name + ")";
     }
 
     public String getName() {
