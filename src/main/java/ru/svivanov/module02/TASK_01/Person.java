36339c562a5dc0f1398b857442608aa66704ae9c
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-11 22:43:54 +0300

    add sorting

--- src/main/java/ru/svivanov/module02/TASK_01/Person.java
+++ src/main/java/ru/svivanov/module02/TASK_01/Person.java
@@ -11,12 +11,39 @@ public class Person {
         this.age = age;
     }
     public void meditation(){
-        System.out.print("I think - it means I exist. " + this +" "+ new Date());
+        System.out.print("I think - it means I exist. " + "(" + this.name + ")" +" "+ new Date());
+    }
+
+    public void sortByName(Person[] personArr){
+        for(int j = 0; j < personArr.length; j++){
+            for(int i = 0; i < personArr.length-1; i++){
+                if (personArr[i].getName().compareTo(personArr[i+1].getName()) > 0){
+                    Person temp = personArr[i];
+                    personArr[i] = personArr[i + 1];
+                    personArr[i + 1] = temp;
+                }
+            }
+        }
+    };
+
+    public void sortByAge(Person[] personArr){
+        for(int j = 0; j < personArr.length; j++){
+            for(int i = 0; i < personArr.length-1; i++){
+                if (personArr[i].getAge() > personArr[i+1].getAge()){
+                    Person temp = personArr[i];
+                    personArr[i] = personArr[i + 1];
+                    personArr[i + 1] = temp;
+                }
+            }
+        }
     }
 
     @Override
     public String toString() {
-        return "(" + name + ")";
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
     }
 
     public String getName() {
--- src/main/java/ru/svivanov/module02/TASK_01/Tests.java
+++ src/main/java/ru/svivanov/module02/TASK_01/Tests.java
@@ -3,6 +3,8 @@ package ru.svivanov.module02.TASK_01;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 
 public class Tests {
     @Test
@@ -30,5 +32,26 @@ public class Tests {
         String cat = new Cat("Murzik", 5, Breed.NEBELUNG, new Person("Stepan", 30)).toString();
         String except = "Cat{name='Murzik', age=5, breed=NEBELUNG, owner=Person{name='Stepan', age=30}}";
         Assert.assertEquals(cat,except);
+    }
+
+    @Test
+    public void personSort(){
+        Person[] personArray = new Person[6];
+        Person person = new Person("Form", 27);
+        personArray[0] = person;
+        personArray[1] = new Person("Brim", 17);
+        personArray[2] = new Person("Arin", 32);
+        personArray[3] = new Person("Tro", 9);
+        personArray[4] = new Person("Limo", 61);
+        personArray[5] = new Person("Miss", 36);
+        System.out.println("Before sorting:");
+        System.out.println(Arrays.toString(personArray));
+        person.sortByName(personArray);
+        System.out.println("Sorting by name:");
+        System.out.println(Arrays.toString(personArray));
+        person.sortByAge(personArray);
+        System.out.println("Sorting by age:");
+        System.out.println(Arrays.toString(personArray));
+
     }
 }
