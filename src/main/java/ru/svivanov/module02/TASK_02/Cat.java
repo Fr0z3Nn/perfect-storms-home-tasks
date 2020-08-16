6bde6196ac3f7794395d27fa0e20d247613cbd3c
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-16 16:13:27 +0300

    task01

---
+++ src/main/java/ru/svivanov/module02/TASK_02/Cat.java
@@ -0,0 +1,22 @@
+package ru.svivanov.module02.TASK_02;
+
+public class Cat {
+    String name;
+    int age;
+    String breed;
+
+    public Cat(String name, int age, String breed) {
+        this.name = name;
+        this.age = age;
+        this.breed = breed;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/ru/svivanov/module02/TASK_02/Main.java
@@ -0,0 +1,13 @@
+package ru.svivanov.module02.TASK_02;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cat cat1 = new Cat("Copper", 1, "DJ");
+        Cat cat2 = new Cat("Silver", 2, "PRO-DJ");
+        Cat cat3 = new Cat("Gold", 3, "NOOB-DJ");
+        System.out.println(cat1);
+        System.out.println(cat2);
+        System.out.println(cat3);
+    }
+}
