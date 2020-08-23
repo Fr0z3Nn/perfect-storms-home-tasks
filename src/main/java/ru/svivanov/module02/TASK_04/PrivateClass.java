aba21c248f65b14602be18c3e543e886d8e17b35
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-23 12:08:18 +0300

    first-task

---
+++ src/main/java/ru/svivanov/module02/TASK_04/PrivateClass.java
@@ -0,0 +1,19 @@
+package ru.svivanov.module02.TASK_04;
+
+public class PrivateClass {
+    private String name;
+    private int age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
