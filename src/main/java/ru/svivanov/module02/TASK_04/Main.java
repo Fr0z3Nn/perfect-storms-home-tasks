be43584ca8307eba2ecc9d7b3c6859d649f667f2
Fr0z3Nn <luckyjohn057@gmail.com>
2020-08-23 12:11:32 +0300

    static count

---
+++ src/main/java/ru/svivanov/module02/TASK_04/Main.java
@@ -0,0 +1,13 @@
+package ru.svivanov.module02.TASK_04;
+
+public class Main {
+    public static void main(String[] args) {
+        PrivateClass p1 = new PrivateClass();
+        PrivateClass p2 = new PrivateClass();
+        PrivateClass p3 = new PrivateClass();
+        PrivateClass p4 = new PrivateClass();
+        PrivateClass p5 = new PrivateClass();
+        PrivateClass p6 = new PrivateClass();
+        System.out.println(PrivateClass.count);
+    }
+}
