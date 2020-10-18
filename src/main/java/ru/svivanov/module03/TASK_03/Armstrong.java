352999f6360f19018790f7a147e520f14df3a9ea
Fr0z3Nn <luckyjohn057@gmail.com>
2020-10-18 18:29:52 +0300

    armstrong

---
+++ src/main/java/ru/svivanov/module03/TASK_03/Armstrong.java
@@ -0,0 +1,30 @@
+package ru.svivanov.module03.TASK_03;
+
+import java.util.stream.LongStream;
+
+public class Armstrong {
+    private static void getNumbers(long value) {
+        LongStream.range(1, value).filter(n -> {
+            String numString = String.valueOf(n);
+            long sum = 0;
+            for (char number : numString.toCharArray()) {
+                sum += Math.pow(Integer.parseInt(String.valueOf(number)), numString.length());
+            }
+            return n == sum;
+        }).forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        long a = System.currentTimeMillis();
+        getNumbers(1000);
+        long b = System.currentTimeMillis();
+        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576);
+        System.out.println("time = " + (b - a) / 1000);
+
+        a = System.currentTimeMillis();
+        getNumbers(9_223_372_036_854_775_807L);
+        b = System.currentTimeMillis();
+        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1048576);
+        System.out.println("time = " + (b - a) / 1000);
+    }
+}
